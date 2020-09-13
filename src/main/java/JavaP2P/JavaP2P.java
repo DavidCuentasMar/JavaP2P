0057630bc4db23fd3f2fabdee91407bc53e1efd2
DavidCuentasMar <davidandrescuentasmar@gmail.com>
2020-09-13 14:39:31 -0500

    Init Main Class

---
+++ src/main/java/JavaP2P/JavaP2P.java
@@ -0,0 +1,6 @@
+package JavaP2P;
+public class JavaP2P {
+    public static void main(String[] args){
+        System.out.println("49f681a1ad36229b88935166b89e2e6e08b8b169bef2e3daef07e910d9e6bcb7");
+    }
+}
